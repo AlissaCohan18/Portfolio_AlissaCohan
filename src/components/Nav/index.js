befc7409ab2b52d9f319ee920f6a1c46889d9d5e
Alissa Cohan <alissacohan18@gmail.com>
2023-03-24 18:12:44 -0400

    added Nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container-fluid">
+        <a className="navbar-brand">Alissa Cohan</a>
+
+        <form class="d-flex">
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  About Me
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
