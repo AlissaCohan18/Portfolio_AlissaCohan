20b0d46ed6a4fbcd6f7276fc6700b8ba62a5499b
Alissa Cohan <alissacohan18@gmail.com>
2023-03-25 11:20:08 -0400

    added state to dictate display

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
-import Nav from './components/Nav';
-import Header from './components/Header';
-import Project from './components/Project';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Project from "./components/Project";
+import Footer from "./components/Footer";
 
 function App() {
- 
+  const [selectedTab, setSelectedTab] = useState("aboutMe");
+
   return (
     <div>
-      <Nav
-      ></Nav>
-     <Header/>
-     <Project/>
-     <Footer/>
+      <Nav setSelectedTab={setSelectedTab}></Nav>
+      <main>
+        {selectedTab==='aboutMe' && <Header />}
+        {selectedTab==='portfolio' && <Project />}
+        {/* {<Resume />} */}
+      </main>
+        <Footer />
     </div>
   );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react";
 
-function Nav() {
+function Nav(props) {
+  const {
+   setSelectedTab
+  } = props;
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container-fluid">
@@ -10,22 +14,22 @@ function Nav() {
           <div className="collapse navbar-collapse" id="navbarColor01">
             <ul className="navbar-nav me-auto">
               <li className="nav-item">
-                <a className="nav-link active" href="#">
+                <a className="nav-link active" href="#" onClick={() => setSelectedTab('aboutMe')}>
                   About Me
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link" href="#" onClick={() => setSelectedTab('portfolio')}>
                   Portfolio
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link" href="#" onClick={() => setSelectedTab('contact')}>
                   Contact
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link" href="#" onClick={() => setSelectedTab('resume')}>
                   Resume
                 </a>
               </li>
