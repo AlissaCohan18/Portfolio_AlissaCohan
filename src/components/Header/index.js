d46322a8350f082365a6cf762d07d63151f9b21b
Alissa Cohan <alissacohan18@gmail.com>
2023-03-25 12:06:38 -0400

    updated avatar and content in About Me

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import avatarImage from "../../assets/avatar/ga.webp";
+import avatarImage from "../../assets/avatar/avatar.png";
 
 function Header() {
   return (
@@ -9,10 +9,12 @@ function Header() {
       <div>
         <p>
           I'm an engineer with a passion for learning and leaving everything I
-          touch better than I found it. My experience includes C++, M and G code
-          for programing CNC lathes, and currently working towards a MERN
-          Certification from Georgia Tech Websites I have worked on during the
-          bootcamp are shown below...
+          touch better than I found it. My computer programming experience
+          includes: working towards a MERN Stack Certification from Georgia Tech
+          which will be achieved in April 2023, M and G code used to program CNC
+          lathes for Lockheed Martin, and C++ fundamentals. On the portfolio
+          tab, you will find details about the websites I have built during my
+          time at Georgia Tech.
         </p>
       </div>
     </section>
